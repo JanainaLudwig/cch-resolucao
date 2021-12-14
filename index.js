4ef4ccbbcf6f052f75886f02683c04ffeacd49ae
Janaina <janainaludwig@gmail.com>
2021-12-13 22:44:00 -0300

    prompt array of ints

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const prompt = require('prompt');
 const getOrdered = require('./order')
 
 let numbers = [1, 3, 6, 2, 0];
 
-console.log(getOrdered(numbers))
+prompt.start();
+
+
+prompt.get(['numbers'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    numbers = result.numbers.split(',').map(num => parseInt(num))
+    console.log('  Numbers: ' + numbers);
+    
+    console.log(getOrdered(numbers))
+});
+
